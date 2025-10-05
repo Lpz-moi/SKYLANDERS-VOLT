abb91b07a8a0f35e98b7d943541b4b86c56519fa
Lpz-moi <youlpz03@gmail.com>
2025-10-05 21:59:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuration Vite optimisée pour Vercel
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ essentiel pour éviter la page blanche
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/',
+  build: {
+    outDir: 'dist',
   },
 });
